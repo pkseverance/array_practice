258bd9b3ac435ddaf2c6a7fc882585af071b9a53
Patricia Severance <patriciaseverance@gmail.com>
2022-08-03 09:48:39 -0600

    Used methods on arrays

--- arrays.rb
+++ arrays.rb
@@ -3,3 +3,17 @@ array2 = [1, 2 ,3 ,4]
 array3 = [11.2, 43.8, 12.5, 1.1]
 array4 = [true, false, false, true]
 
+#Prints length of array1 to console
+puts array1.length
+
+#Removes last element in array2 and prints to console
+array2.pop
+puts array2
+
+#Adds 55.9 to the end of array 3 and prints to console
+array3.push(55.9)
+puts array3
+
+#Removes first element in array4 and prints to console
+array4.shift
+puts array4
